b96d73a91b9151a51b6ecd86440a853bed5a7eaf
coulava <coulava_rahmawati_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:48:34 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,11 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Halo");
+        System.out.println("perkenalkan nama saya Coualava Illona Rahmawati");
+        System.out.println("atau  kalian bisa panggil audy");
+        System.out.println("saya berasal dari mojokerto");
+        System.out.println("saya berasal dari smp islam al akbar");
+        System.out.println("no hp saya yaitu");
+        System.out.println("081357248148");
+    }
+}
